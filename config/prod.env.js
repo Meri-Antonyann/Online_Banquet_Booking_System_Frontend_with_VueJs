16076d69c9cda1eff09a846fe42da3fc3878cc69
Meri Antonyan <meri-antonyan-1997@mail.ru>
2022-05-04 12:51:24 +0400

    just

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://127.0.0.1:8000/api"'
-
+  // API_URL: '"http://127.0.0.1:8000/api"'
+  API_URL: '"https://online-banquet-booking-backend.herokuapp.com/api"'
 }
